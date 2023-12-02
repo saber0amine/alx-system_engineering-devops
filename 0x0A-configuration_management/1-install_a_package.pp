939b33d2f6342ba0fecff6baec75eda64b6785c2
Your Name <you@example.com>
2023-12-02 13:12:04 +0100

    fix devops 2

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,18 @@
 # 1-install_a_package.pp
 
-# Define package resource to install Flask with version 2.1.0
+# Define package resources to install Flask and Werkzeug with specific versions
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
 
+package { 'Werkzeug':
+  ensure   => '2.1.1', # Use the correct version compatible with Flask 2.1.0
+  provider => 'pip3',
+}
+
 # Notify the user about the installation
 notify { 'Flask installation completed':
-  require => Package['Flask'],
+  require => [Package['Flask'], Package['Werkzeug']],
 }
 
