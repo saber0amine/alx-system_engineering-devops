4404f31a2f89791d025817a9c12c494ce3ce40f1
saber0amine <120688951+saber0amine@users.noreply.github.com>
2024-04-10 02:03:12 +0000

    add v4

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,17 @@
 #!/usr/bin/python3
-"""number of subscribers"""
+"""
+Contains the number_of_subscribers function
+"""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """gets the number of subscribers for a given subreddit"""
-
-    url = f'https://www.reddit.com/r/{subreddit}/about.json'
-    res = requests.get(url, headers={'User-Agent': 'TutTrue'}).json()
-    return res.get('data', {}).get('subscribers', 0)
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
