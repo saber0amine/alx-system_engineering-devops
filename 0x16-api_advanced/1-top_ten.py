d67c4acd2dc9b7145c652acc962e1d178c63bca7
saber0amine <120688951+saber0amine@users.noreply.github.com>
2024-04-10 02:00:43 +0000

    v3 added

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,46 +1,19 @@
 #!/usr/bin/python3
-""" querry reddit api for subreddit info"""
+"""get the first 10 hot posts listed for a given subreddit."""
+
 import requests
-import requests.auth
 
 
 def top_ten(subreddit):
-    """ querry reddit api"""
-    sub = subreddit
-    subreddit = "/r/{}/hot".format(sub)
-    temp = "HolbertonPass845"
+    """gets the titles of the first 10 hot posts"""
 
-    secret = "Z4Sa9bA6RRE44qDyhHQiTlW1gd0"
-    client_id = "hy4KvoK0W2iDvw"
-    usr_name = "jgadelugo"
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+    res = requests.get(url, headers={'User-Agent': 'TutTrue'},
+                       allow_redirects=False)
 
-    client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
-    post_data = {"grant_type": "password",
-                 "username": usr_name,
-                 "password": temp}
-
-    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
-    response = requests.post("https://www.reddit.com/api/v1/access_token",
-                             auth=client_auth, data=post_data, headers=headers)
-    auth_json = response.json()
-
-    token_type = auth_json['token_type']
-    access_token = auth_json['access_token']
-
-    headers = {"Authorization": "{} {}".format(token_type, access_token),
-               "User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
-
-    param = {"limit": 10}
-
-    query = "https://oauth.reddit.com{}".format(subreddit)
-    res = requests.get(query, headers=headers, params=param)
-
-    status = res.status_code
-
-    if (status != 200):
-        print("None")
-    else:
-        data = res.json()
-        posts = data["data"]['children']
+    if res.status_code == 200:
+        posts = res.json().get('data', {}).get('children', [])
         for post in posts:
-            print(post['data']['title'])
+            print(post.get('data', {}).get('title', ''))
+    else:
+        print(None)
