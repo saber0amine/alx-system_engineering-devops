56f34a806973cacbd16bfd8b1e2f5e286be26320
saber0amine <120688951+saber0amine@users.noreply.github.com>
2024-04-10 01:57:17 +0000

    add tasks

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-""" function that executes an api get request """
-from requests import get
+"""
+this doc for module
+"""
+import requests
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):
-    """ api request to get number of subscribers """
-    if subreddit is None or not isinstance(subreddit, str):
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data["data"]["subscribers"]
+    else:
         return 0
-    lien = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
-                Gecko/20100101 Firefox/108.0"
-    }
-    response = get(lien, headers=headers).json()
-    subscribers = response.get('data', {}).get('subscribers', 0)
-    return subscribers
--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,28 +1,19 @@
 #!/usr/bin/python3
-""" top_ten module """
-from requests import get
+"""
+this doc for module
+"""
+import requests
+
+headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def top_ten(subreddit):
-    """
-    function that gets titles of first hot posts
-    listed for a subbredit
-    """
-    if subreddit is None:
-        return 0
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
-    headers = {
-        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:108.0) \
-                Gecko/20100101 Firefox/108.0"
-    }
-    params = {
-            "limit": 10
-            }
-    response = get(url, params=params, headers=headers,
-                   allow_redirects=False).json()
-    children = response.get("data", {}).get("children", None)
-    if children:
-        for subject in children:
-            print(subject.get("data").get("title"))
+    """method doc"""
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+    response = requests.get(url, allow_redirects=False, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        for post in data["data"]["children"]:
+            print(post["data"]["title"])
     else:
         print("None")
