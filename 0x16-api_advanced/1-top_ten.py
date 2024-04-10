c8a2f2ca8a84fe179cb6234c1098aa0561412684
saber0amine <120688951+saber0amine@users.noreply.github.com>
2024-04-10 01:58:47 +0000

    add version 2

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,19 +1,46 @@
 #!/usr/bin/python3
-"""
-this doc for module
-"""
+""" querry reddit api for subreddit info"""
 import requests
-
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+import requests.auth
 
 
 def top_ten(subreddit):
-    """method doc"""
-    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        for post in data["data"]["children"]:
-            print(post["data"]["title"])
-    else:
+    """ querry reddit api"""
+    sub = subreddit
+    subreddit = "/r/{}/hot".format(sub)
+    temp = "HolbertonPass845"
+
+    secret = "Z4Sa9bA6RRE44qDyhHQiTlW1gd0"
+    client_id = "hy4KvoK0W2iDvw"
+    usr_name = "jgadelugo"
+
+    client_auth = requests.auth.HTTPBasicAuth(client_id, secret)
+    post_data = {"grant_type": "password",
+                 "username": usr_name,
+                 "password": temp}
+
+    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
+    response = requests.post("https://www.reddit.com/api/v1/access_token",
+                             auth=client_auth, data=post_data, headers=headers)
+    auth_json = response.json()
+
+    token_type = auth_json['token_type']
+    access_token = auth_json['access_token']
+
+    headers = {"Authorization": "{} {}".format(token_type, access_token),
+               "User-Agent": "ChangeMeClient/0.1 by {}".format(usr_name)}
+
+    param = {"limit": 10}
+
+    query = "https://oauth.reddit.com{}".format(subreddit)
+    res = requests.get(query, headers=headers, params=param)
+
+    status = res.status_code
+
+    if (status != 200):
         print("None")
+    else:
+        data = res.json()
+        posts = data["data"]['children']
+        for post in posts:
+            print(post['data']['title'])
